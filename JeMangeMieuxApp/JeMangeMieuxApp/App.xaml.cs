4b1592db11127609c7f946911556a0ea9a351f91
smartykev <kguilloud.pro@gmail.com>
2022-07-28 14:43:32 +0200

    From Scratch

---
+++ JeMangeMieuxApp/JeMangeMieuxApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using JeMangeMieuxApp.Services;
+using JeMangeMieuxApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JeMangeMieuxApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
